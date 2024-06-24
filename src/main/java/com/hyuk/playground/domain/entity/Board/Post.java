e757ce47dca2f06d53bc7b9fe502150859e3a591
sanghyuk <ish775800@gmail.com>
2024-06-24 21:59:17 +0900

    초기 설정

---
+++ src/main/java/com/hyuk/playground/domain/entity/Board/Post.java
@@ -0,0 +1,25 @@
+package com.hyuk.playground.domain.entity.Board;
+
+import com.hyuk.playground.global.entity.BaseTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Post extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false, length = 100)
+    private String author;
+}
